f85e6d800687f19ea45c59517e658631f0376d3a
Tabunkin Vadim <tabunkin.vadim@icloud.com>
2022-10-09 13:04:27 +0400

    Initial Commit

---
+++ SwiftUIHomework/SwiftUIHomeworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIHomeworkApp.swift
+//  SwiftUIHomework
+//
+//  Created by Табункин Вадим on 09.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIHomeworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
